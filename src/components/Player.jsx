3cb58f3ac8c52f017403d897273a0b0511c40e4d
MikelDev <millymike599@gmail.com>
2024-08-21 21:15:21 +0100

    add state to game

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name"> {name} </span>
+        ) : (
+          <input type="text" required />
+        )}
+
+        <span className="player-symbol"> {symbol} </span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
+}
