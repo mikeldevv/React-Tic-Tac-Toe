78d137a957aa2dd60938576ae144c5928e06b423
MikelDev <millymike599@gmail.com>
2024-08-22 17:59:01 +0100

    two way binding with event.target.value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name"> {name} </span>
+          <span className="player-name"> {playerName} </span>
         ) : (
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         )}
 
         <span className="player-symbol"> {symbol} </span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
